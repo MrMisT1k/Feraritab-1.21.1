4d2cae03b1ea512b0dd0e52fcf39b218a1689ba4
Curb21 <karklinartem2@gmail.com>
2025-12-27 15:10:43 +0300

    Добавил блок говна и текстуру к нему

--- src/main/java/net/debilX/feraritab/block/ModBlocks.java
+++ src/main/java/net/debilX/feraritab/block/ModBlocks.java
@@ -15,24 +15,29 @@ import net.minecraftforge.registries.RegistryObject;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCK =
+    public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, feraritab.MOD_ID);
 
     public static final RegistryObject<Block> PISA_BLOCK = registerBlock("pisa_block",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCK.register(name, block);
+    public static final RegistryObject<Block> SHIT_BLOCK = registerBlock("shit_block",
+            ()-> new Block(BlockBehaviour.Properties.of()
+                    .strength(2f).sound(SoundType.MUD)));
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
         return toReturn;
     }
 
-    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block){
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
     public static void register(IEventBus eventBus){
-        BLOCK.register(eventBus);
+        BLOCKS.register(eventBus);
     }
 }
