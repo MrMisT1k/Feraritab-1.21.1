4a9397e557a7bf177783ed76319ee812fc5e8dfb
MrMisT1k <xatzmax@gmail.com>
2025-12-27 19:05:33 +0200

    Руда + обнова текстур + нужен фикс руда не выпадет

--- src/main/java/net/debilX/feraritab/block/ModBlocks.java
+++ src/main/java/net/debilX/feraritab/block/ModBlocks.java
@@ -2,9 +2,11 @@ package net.debilX.feraritab.block;
 
 import net.debilX.feraritab.feraritab;
 import net.debilX.feraritab.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,13 +22,16 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> PISA_BLOCK = registerBlock("pisa_block",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+                    .strength(1f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
 
     public static final RegistryObject<Block> SHIT_BLOCK = registerBlock("shit_block",
             ()-> new Block(BlockBehaviour.Properties.of()
                     .requiresCorrectToolForDrops()
                     .strength(2f).sound(SoundType.MUD)));
 
+    public static final RegistryObject<Block> PISA_ORE = registerBlock("pisa_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2,4), BlockBehaviour.Properties.of()
+                    .strength(1f).requiresCorrectToolForDrops()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
