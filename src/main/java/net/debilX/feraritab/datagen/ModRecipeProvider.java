08ae4c6327739951a69b3fe313bc1d4940d72ebf
Curb21 <karklinartem2@gmail.com>
2025-12-29 15:53:08 +0300

    Сделал DataGen

--- src/main/java/net/debilX/feraritab/datagen/ModRecipeProvider.java
+++ src/main/java/net/debilX/feraritab/datagen/ModRecipeProvider.java
@@ -1,4 +1,63 @@
 package net.debilX.feraritab.datagen;
 
-public class ModRecipeProvider {
+import net.debilX.feraritab.block.ModBlocks;
+import net.debilX.feraritab.feraritab;
+import net.debilX.feraritab.item.ModItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public ModRecipeProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pRegistries) {
+        super(pOutput, pRegistries);
+    }
+
+    @Override
+    protected void buildRecipes(RecipeOutput pRecipeOutput) {
+        List<ItemLike> PISA_SMELTABLES = List.of(ModItems.RAW_PISA.get());//Сюда помещаем все предметы, которые при переплавке дают raw_pisa
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.SHIT_BLOCK.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', Items.BROWN_DYE)
+                .unlockedBy(getHasName(Items.BROWN_DYE), has(Items.BROWN_DYE)).save(pRecipeOutput, feraritab.MOD_ID + "shit_block_from_brown_dye");
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.BLUE_BAMBOO.get(), 1)
+                .requires(Items.BAMBOO)
+                .requires(Items.BROWN_DYE)
+                .unlockedBy(getHasName(Items.BAMBOO), has(Items.BAMBOO))
+                .unlockedBy(getHasName(Items.BROWN_DYE), has(Items.BROWN_DYE)).save(pRecipeOutput);
+
+        oreSmelting(pRecipeOutput, PISA_SMELTABLES, RecipeCategory.MISC, ModItems.PISA.get(), 0.25f, 200, "pisa");
+        oreBlasting(pRecipeOutput, PISA_SMELTABLES, RecipeCategory.MISC, ModItems.PISA.get(), 0.25f, 200, "pisa");
+
+
+    }
+    protected static void oreSmelting(RecipeOutput recipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                      float pExperience, int pCookingTIme, String pGroup) {
+        oreCooking(recipeOutput, RecipeSerializer.SMELTING_RECIPE, SmeltingRecipe::new, pIngredients, pCategory, pResult,
+                pExperience, pCookingTIme, pGroup, "_from_smelting");
+    }
+
+    protected static void oreBlasting(RecipeOutput recipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                      float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(recipeOutput, RecipeSerializer.BLASTING_RECIPE, BlastingRecipe::new, pIngredients, pCategory, pResult,
+                pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
+
+    protected static <T extends AbstractCookingRecipe> void oreCooking(RecipeOutput recipeOutput, RecipeSerializer<T> pCookingSerializer, AbstractCookingRecipe.Factory<T> factory,
+                                                                       List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+        for(ItemLike itemlike : pIngredients) {
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult, pExperience, pCookingTime, pCookingSerializer, factory).group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(recipeOutput, feraritab.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
+        }
+    }
 }
